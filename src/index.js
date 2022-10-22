ada7a894fdafa13081ea0ed56e5a5c6210eb4cd8
masamichan <positivemasa031@gmail.com>
2022-10-22 18:20:22 +0900

    Big change in package.json

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/asset/css/custom.css";
+import "../src/asset/css/animate.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
